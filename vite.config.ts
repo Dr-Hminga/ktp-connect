825142a2f34834d951259b844fb50ed83c93cedc
Dr-Hminga <hmingchungnunga96@gmail.com>
2026-03-24 16:51:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/ktp-connect/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
