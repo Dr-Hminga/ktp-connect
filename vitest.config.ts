00ae8646c4c032c1389d554cc0eb37c12fa25a8e
Dr-Hminga <hmingchungnunga96@gmail.com>
2026-03-24 16:47:32 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,4 +13,8 @@ export default defineConfig({
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
+  export default defineConfig({
+  base: '/ktp-connect/',
+  // ... other config
+})
 });
